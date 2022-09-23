3bbb6b3d4e599be4b67100f2b4780287510e7750
Shelakh <aarribas@student.42malaga.com>
2022-09-23 15:20:37 +0200

    test

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 /*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 13:53:10 by aarribas          #+#    #+#             */
-/*   Updated: 2022/09/23 14:51:55 by aarribas         ###   ########.fr       */
+/*   Updated: 2022/09/23 15:20:27 by aarribas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include "philo.h"
+
+int	main(int ac, char av[])
 {
+	test.c
 }
